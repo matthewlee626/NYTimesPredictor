762b699f5051bf5b733537c4144faae041fac3f1
MatthewLi36177 <matthewli36177@gmail.com>
2019-07-16 11:41:26 -0700

    valid dates

--- googlejsontocsv.py
+++ googlejsontocsv.py
@@ -4,8 +4,8 @@ import requests
 
 # using isbnData.json
 
-START = 1000
-END = 2000
+START = 0
+END = 2505
 
 def getISBNs():
     with open('listOfMissingDatesISBNs.csv', 'r') as csvList:
@@ -18,9 +18,11 @@ def getISBNs():
 
 
 def getjson():
-    PATH = "C:\\Users\\matth\\Desktop\\NYTimesPredictor\\GoogleJSON\\googleBooksAPIData2000.json"
-    jsonList = open(PATH, 'r')
-    isbnInfo = [line for line in jsonList if json.loads(line)['totalItems'] != 0]  # converting json into a list, removing the empty jsons
+    PATHS = ["0150.json", "0300.json", "0450.json", "0600.json", "0750.json", "0900.json", "1000.json", "2000.json", "2170.json", "2340.json", "2505.json"]
+    isbnInfo = []
+    for i in range(len(PATHS)):
+        jsonList = open(PATHS[i], 'r')
+        isbnInfo.extend([line for line in jsonList if json.loads(line)['totalItems'] != 0])   # converting json into a list, removing the empty jsons
     return isbnInfo
 
 
@@ -34,7 +36,7 @@ with open('publishDates.csv', mode='w', newline='', encoding="utf-8") as output:
 
     writecount = 0
 
-    header = ['ISBN', 'Publish Date']  # values we need
+    header = ['ISBN', 'Publish Date', 'Season', 'Season (int)']  # values we need
 
     csvwriter.writerow(header)
 
@@ -44,9 +46,7 @@ with open('publishDates.csv', mode='w', newline='', encoding="utf-8") as output:
     for jsonLine in masterList:
         j = json.loads(jsonLine) # convert a string value into its corresponding json
         #print(j)
-        print("writing csv line " + str(writecount))
-        writecount += 1
-        # testing purposes
+
 
         masterISBN = ISBNs[index]
         index += 1
@@ -63,23 +63,32 @@ with open('publishDates.csv', mode='w', newline='', encoding="utf-8") as output:
             mPublishDate = "Date 0"
 
         season = ""
+        season_date = 0
 
         if len(mPublishDate) == 10:
             haveDates += 1
             month = int(mPublishDate[5:7])
             if month <= 2 or month == 12:
                 season = "Winter"
+                season_date = 0
             elif 3 <= month <= 5:
                 season = "Spring"
+                season_date = 1
             elif 6 <= month <= 8:
                 season = "Summer"
+                season_date = 2
             elif 9 <= month <= 11:
                 season = "Autumn"
+                season_date = 3
         else:
             season = "Unknown"
 
-        values = [masterISBN, mPublishDate, season]
+        values = [masterISBN, mPublishDate, season, season_date]
 
-        csvwriter.writerow(values)
+        if len(mPublishDate) == 10:
+            print("writing csv line " + str(writecount))
+            writecount += 1
+            # testing purposes
+            csvwriter.writerow(values)
 
     print(haveDates)
